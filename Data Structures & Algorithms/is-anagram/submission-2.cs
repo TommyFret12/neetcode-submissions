7a17e5b844673d0d08840a7c77340665117124ae
TommyFret12 <tommyeforrest@icloud.com>
2026-08-18 13:14:22 +0100

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length){
+            return false;
+        }
+        Dictionary<char,int> myDictS = new Dictionary<char,int>();
+        Dictionary<char,int> myDictT = new Dictionary<char,int>();
+        for(int i = 0; i<s.Length; i++){
+            if(!myDictS.ContainsKey(s[i])){
+                myDictS[s[i]] = 1;
+            }
+            else{
+                myDictS[s[i]] +=1;
+            }
+            if(!myDictT.ContainsKey(t[i])){
+                myDictT[t[i]] = 1;
+            }
+            else{
+                myDictT[t[i]] +=1;
+            }
+            
+        }
+        return myDictS.Count == myDictT.Count && !(myDictS.Except(myDictT).Any());
+
+    }
+}
